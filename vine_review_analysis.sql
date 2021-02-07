e2f25c8f1f037863535fa69df229910365fca182
Donnie <Donnell.Jackson@outlook.com>
2021-02-07 14:00:50 -0600

    updating anaylsis with .sql files and notebooks

--- vine_review_analysis.sql
+++ vine_review_analysis.sql
@@ -1,35 +1,3 @@
--- Creating Schema 
-CREATE TABLE review_id_table (
-  review_id TEXT PRIMARY KEY NOT NULL,
-  customer_id INTEGER,
-  product_id TEXT,
-  product_parent INTEGER,
-  review_date DATE -- this should be in the formate yyyy-mm-dd
-);
-
--- This table will contain only unique values
-CREATE TABLE products_table (
-  product_id TEXT PRIMARY KEY NOT NULL UNIQUE,
-  product_title TEXT
-);
-
--- Customer table for first data set
-CREATE TABLE customers_table (
-  customer_id INT PRIMARY KEY NOT NULL UNIQUE,
-  customer_count INT
-);
-
--- vine table
-CREATE TABLE vine_table (
-  review_id TEXT PRIMARY KEY,
-  star_rating INTEGER,
-  helpful_votes INTEGER,
-  total_votes INTEGER,
-  vine TEXT,
-  verified_purchase TEXT
-);
-
-
 --Create table of vine reviews with 20 or more votes from vine_table
 SELECT *
 INTO vote_count_20
@@ -54,18 +22,3 @@ INTO non_vine_program_reviews
 FROM vote_50_percent 
 WHERE vine = 'N';
 
--- 
-
-
-
-(SELECT COUNT(*) AS total_paid_votes FROM vine_program_reviews)
-UNION
-(SELECT COUNT(*) AS total_unpaid_reviews FROM non_vine_program_reviews);
-
-SELECT vote_paid non_vote FROM ( SELECT COUNT(*) 
-									 	AS vote_paid 
-									 	FROM vine_program_reviews
-									 	UNION
-									 	SELECT COUNT(*)
-									 	AS non_vote
-									 	FROM non_vine_program_reviews) AS x;
